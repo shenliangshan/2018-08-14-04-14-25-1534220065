9e80838a557866944bd6baeca9dadef73a2a5da2
shenliangshan <shenliangshan_hust@163.com>
2019-06-01 11:40:46 +0800

    second

--- main/main.js
+++ main/main.js
@@ -1,4 +1,18 @@
-module.exports = function main() {
-    console.log("Debug Info");
-    return 'Hello World!';
+module.exports = function main(str) {
+    // Array of lcd light 
+    top = ["._.", "...", "._.", "._.", "...", "._.", "._.", "._.", "._.", "._."];
+    middle = ["|.|", "..|", "._|", "._|", "|_|", "|_.", "|_.", "..|", "|_|", "|_|"];
+    bottom = ["|_|", "..|", "|_.", "._|", "..|", "._|", "|_|", "..|", "|_|", "..|"];
+
+    res_t = [];
+    res_m = [];
+    res_b = [];
+
+    for(var i = 0 ; i < str.length ;  i++){
+        var n = parseInt(str[i]);
+        res_t.push(top[n]);
+        res_m.push(middle[n]);
+        res_b.push(bottom[n]);
+      }
+      return res_t.join(' ') + '\n' + res_m.join(' ') + '\n' + res_b.join(' ') + '\n';
 };
